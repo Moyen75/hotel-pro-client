078de795737cd590bdf6da27270220753a1c08fe
moyen75 <moyenislam75@gmail.com>
2023-01-10 23:31:09 +0600

    login page design done

--- src/shared/Login/Login.js
+++ src/shared/Login/Login.js
@@ -8,26 +8,42 @@ import useAuth from '../../hooks/useAuth';
 import { useLocation, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import Footer from '../../pages/Home/Footer/Footer';
+import { useEffect } from 'react';
 
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [err, setErr] = useState(false)
     const { googleSignIn, emailSignIn } = useAuth()
     let navigate = useNavigate();
     let location = useLocation();
 
-
-
+    useEffect(() => {
+        if (err) {
+            swal(
+                'ooppps!',
+                'Invalid email!',
+                'error'
+            )
+            setErr(false)
+            setEmail("")
+        }
+    }, [err])
     const handleEmail = e => {
+        const email = e.target.value;
+        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!email.match(mailformat)) return setErr(true)
         setEmail(e.target.value)
+
     }
     const handlePassword = e => {
         setPassword(e.target.value)
     }
     const handleOnSubmit = e => {
         e.preventDefault()
-        emailSignIn(email, password, success, navigate, location)
+        console.log(email, password)
+        // emailSignIn(email, password, success, navigate, location)
     }
     const success = () => {
         swal(
@@ -42,9 +58,30 @@ const Login = () => {
                 <div className="login-box">
                     <div className="login-cover"></div>
                     <div className="login-shadow"></div>
-                    <div className="link-content">
-                        <div className="link-form">
-                            <h3 className="link-logo"></h3>
+                    <div className="login-content">
+
+                        <div className="login-form">
+                            <h3 className="login-logo">
+                                <i class="fa-solid fa-key"></i>
+                            </h3>
+                            <h2>sign in</h2>
+                            <div className="input-box">
+                                <input onBlur={handleEmail} name="email" type="text" required />
+                                <i class="fa-solid fa-envelope"></i>
+                                <span>Email</span>
+                            </div>
+                            <div className="input-box">
+                                <input onBlur={handlePassword} name="password" type="password" required />
+                                <i class="fa-solid fa-lock"></i>
+                                <span>password</span>
+                            </div>
+                            <div className="login-links">
+                                <a href="#">Forgot Password <i class="fa-solid fa-question"></i></a>
+                                <a href="#">Sign up <i class="fa-solid fa-user-plus"></i></a>
+                            </div>
+                            <div className="input-box">
+                                <input type="submit" onClick={handleOnSubmit} value="Signin" />
+                            </div>
                         </div>
                     </div>
                 </div>
