63741ad8779f37c6101ce3d110d23a2c5ca246a5
moyen75 <moyenislam75@gmail.com>
2023-01-10 21:29:58 +0600

    login page start

--- src/shared/Login/Login.js
+++ src/shared/Login/Login.js
@@ -37,43 +37,18 @@ const Login = () => {
         )
     }
     return (
-        <Box sx={{ backgroundColor: "#f4f4f4" }}>
-            <Container >
-                <Box sx={{ paddingY: "70px" }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={6}>
-                            <Box sx={{ display: { xs: 'none', md: "inline" } }}>
-                                <img style={{ width: '100%' }} src={loginImg} alt="" />
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <Box sx={{ maxWidth: "600px" }}>
-                                <h1> WELCOME BACK</h1>
-                                <p>log in to continue</p>
-                                <form onSubmit={handleOnSubmit}>
-                                    <label htmlFor="email"><input className='login-input'
-                                        onBlur={handleEmail}
-                                        type="email" placeholder='Enter Email' /><span className='input'><i class="fas fa-user"></i></span></label>
-                                    <label htmlFor="password"><input
-                                        onBlur={handlePassword}
-                                        className='login-input' type="password" placeholder='Enter Password' /><span className='input'><i class="fas fa-key"></i></span></label>
-                                    <input className='submit' style={{ display: "block", margin: "10px auto" }} type='submit' value='LOG IN' />
-                                </form>
-                            </Box>
-                            <Typography sx={{ width: '35px', height: "35px", borderRadius: "50%", backgroundColor: 'gray', color: "white", lineHeight: '35px', margin: ' 10px auto' }}>
-                                or
-                            </Typography>
-                            <button style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '0 auto', padding: '5px' }} onClick={() => googleSignIn(success, navigate, location)}>
-                                <img width="20px" src={google} alt="" /><span >Sign in with Google</span>
-                            </button>
-                            <Box sx={{ marginY: '15px' }}>
-                                <span>Don't Have an Account?</span> <Link className='link' to='/register'>Register Now</Link>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Container>
-            <Footer></Footer>
+        <Box sx={{ backgroundColor: "#cfd1e1", overflow: "hidden", display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
+            <div className="login-container">
+                <div className="login-box">
+                    <div className="login-cover"></div>
+                    <div className="login-shadow"></div>
+                    <div className="link-content">
+                        <div className="link-form">
+                            <h3 className="link-logo"></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Box>
     );
 };
